185a7883996e659ab54b2f029473a27f2c6beffc
gskang <gskang2002@gmail.com>
2025-03-14 14:48:09 +0800

    testing

---
+++ Core/BSP/Src/bsp_servo.c
@@ -0,0 +1,20 @@
+/*
+ * bsp_servo.c
+ *
+ *  Created on: Mar 13, 2025
+ *      Author: gskan
+ */
+
+
+
+
+#include <bsp_servo.h>
+#include "board_lib.h"
+
+
+void servo_init()
+{
+	  HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_1);
+	  HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_3);
+}
+
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -110,6 +110,7 @@ int main(void)
   MX_ADC3_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
+  MX_TIM8_Init();
   MX_RNG_Init();
   MX_TIM2_Init();
   MX_CRC_Init();
