9f315f9e34dbdd486aa26cfc128d715e11e95952
ChingWhee <wongchingwhee@gmail.com>
2025-03-20 22:41:48 +0800

    Add new robot configs for hero, standard and sentry

---
+++ Core/robot_config/robot_config_kirbee.h
@@ -0,0 +1,13 @@
+/*
+ * robot_config_kirbee.h
+ *
+ *  Created on: Mar 20, 2025
+ *      Author: cw
+ */
+
+#ifndef ROBOT_CONFIG_ROBOT_CONFIG_KIRBEE_H_
+#define ROBOT_CONFIG_ROBOT_CONFIG_KIRBEE_H_
+
+
+
+#endif /* ROBOT_CONFIG_ROBOT_CONFIG_KIRBEE_H_ */
