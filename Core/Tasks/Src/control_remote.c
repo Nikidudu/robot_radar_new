2afc7947a66d4684eb5758d2cb0ad6158d9991d3
Thia Yang Han <E1398615@u.nus.edu>
2025-06-02 17:16:48 +0800

    remote x limiting

--- Core/Tasks/Src/control_remote.c
+++ Core/Tasks/Src/control_remote.c
@@ -21,6 +21,7 @@ extern gun_control_t launcher_ctrl_data;
 extern gimbal_control_t gimbal_ctrl_data;
 extern uint8_t g_safety_toggle;
 extern uint8_t launcher_safety_toggle;
+float remote_idle = 3;
 
 
 void remote_control_input() {
@@ -96,6 +97,10 @@ void remote_gimbal_input() {
 		gimbal_ctrl_data.enabled = 0;
 	} else {
 		gimbal_ctrl_data.enabled = 1;
+		if (fabs(g_remote_cmd.right_x) < remote_idle)
+		{
+			g_remote_cmd.right_x = 0;
+		}
 		float pitch_remote = ((float) g_remote_cmd.right_y / 660) * PITCH_INVERT
 				* REMOTE_PITCH_SPEED;
 		float yaw_remote = ((float) g_remote_cmd.right_x / 660) * YAW_INVERT
--- Core/Tasks/Src/gimbal_control_task.c
+++ Core/Tasks/Src/gimbal_control_task.c
@@ -223,10 +223,10 @@ void gimbal_control(motor_data_t *pitch_motor, motor_data_t *yaw_motor) {
 	}
 	xSemaphoreTake(gimbal_ctrl_data.yaw_semaphore,portMAX_DELAY);
 	gimbal_ctrl_data.delta_yaw -= turn_ang;
-	if (fabs(gimbal_ctrl_data.delta_yaw) < yaw_drift)
-	{
-		gimbal_ctrl_data.delta_yaw = 0;
-	}
+//	if (fabs(gimbal_ctrl_data.delta_yaw) < yaw_drift)
+//	{
+//		gimbal_ctrl_data.delta_yaw = 0;
+//	}
 	yangle_pid(gimbal_ctrl_data.delta_yaw, 0, yaw_motor,
 			imu_heading.yaw, &prev_yaw,0);
 	xSemaphoreGive(gimbal_ctrl_data.yaw_semaphore);
