e8c345b38b8738a992eeff573b96962c5fbcbb8b
kyrri0 <kwinter5676@gmail.com>
2025-06-17 15:22:53 +0800

    kirbee 1v1 tuning again

--- Core/robot_config/robot_config_kirbee.h
+++ Core/robot_config/robot_config_kirbee.h
@@ -63,7 +63,7 @@
  * E.g. 0.5 = 50% max speed
  */
 #define LVL_TUNING
-#define LV1_MAX_SPEED 		    0.38//0.20 // for 1v1
+#define LV1_MAX_SPEED 		    0.34//0.20 // for 1v1
 #define LV2_MAX_SPEED  			0.22
 #define LV3_MAX_SPEED			0.24
 #define LV4_MAX_SPEED			0.26
@@ -83,7 +83,7 @@
  *  2.00 - Extremely Fast Acceleration
  */
 //Chassis Acceleration
-#define LV1_MAX_ACCEL			3.5//1.5 // for 1v1
+#define LV1_MAX_ACCEL			2.5//1.5 // for 1v1
 #define LV2_MAX_ACCEL			2.0
 #define LV3_MAX_ACCEL			2.0
 #define LV4_MAX_ACCEL			2.0
