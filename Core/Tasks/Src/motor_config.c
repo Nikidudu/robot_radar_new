91c8d3e93025d06385844bf06cfae1e160e9adaa
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-06-26 12:13:26 +0800

    update error in pitch overheat code

--- Core/Tasks/Src/motor_config.c
+++ Core/Tasks/Src/motor_config.c
@@ -792,7 +792,7 @@ uint16_t check_motors() {
 		dm_set_pitch_motor();
 	}
 	else {
-		if (g_pitch_motor.raw_data.temp > HITEMP_WARNING) {
+		if (dm_pitch_motor.para.Tcoil > HITEMP_WARNING) {
 			motor_temp_bz(3, 1);
 		}
 	}
