07d399900e7e39bb15d80fd2ab856eb07bc68fee
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-06-26 12:34:21 +0800

    increase kirbee firing speed

--- Core/robot_config/robot_config_kirbee.h
+++ Core/robot_config/robot_config_kirbee.h
@@ -179,7 +179,7 @@
 #define CHASSIS_POWER_DELTA_LIM 0.3   // limit change for the automatic rpm adjusting code
 
 
-#define PROJECTILE_SPEED_RATIO	300//340//355//360								//rpm per m/s of the friction wheels ish don't think this will work well lmao
+#define PROJECTILE_SPEED_RATIO	310//340//355//360								//rpm per m/s of the friction wheels ish don't think this will work well lmao
 #define FEEDER_SPEED_RATIO		-8								//projectiles per round of the feeder
 
 /*********************** MANUAL CONTROL CONFIGURATION *******************/
