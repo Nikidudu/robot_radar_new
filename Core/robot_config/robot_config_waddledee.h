7051c53c0625f13be9a8531ceebf52db4f68429a
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-06-26 12:31:43 +0800

    add 3v3 values for kirbee + half pitch sens

--- Core/robot_config/robot_config_waddledee.h
+++ Core/robot_config/robot_config_waddledee.h
@@ -37,7 +37,7 @@
 #define REMOTE_PITCH_SPEED 	 			-0.1//0.005		//Speed of gimbal pitch turning
 
 #define MOUSE_X_SENSITIVITY		(300 * REMOTE_YAW_SPEED)				//Speed of yaw turning with mouse, dependent on above speed
-#define MOUSE_Y_SENSITIVITY 	(200 * REMOTE_PITCH_SPEED)				//Speed of pitch turning with mouse,  dependent on above speed
+#define MOUSE_Y_SENSITIVITY 	(100 * REMOTE_PITCH_SPEED)				//Speed of pitch turning with mouse,  dependent on above speed
 
 
 #define OVERHEAT_MARGIN 2
