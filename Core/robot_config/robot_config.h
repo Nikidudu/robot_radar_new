71ba4208e47ba38e9fdbb7c3073527a966bfd3d0
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-07-16 21:58:26 +0800

    disable beeping. change default to remote mode

--- Core/robot_config/robot_config.h
+++ Core/robot_config/robot_config.h
@@ -13,10 +13,10 @@
 //#include "robot_config_roburt.h"
 //#include "robot_config_kirby.h"
 
-//#include "robot_config_kirbee.h"
+#include "robot_config_kirbee.h"
 //#include "robot_config_sentry.h"
 //#include "robot_config_TALI_LK.h"
-#include "robot_config_waddledee.h"
+//#include "robot_config_waddledee.h"
 
 //#include "robot_config_CF2.h"
 //#include "robot_config_DS.h"
@@ -32,7 +32,7 @@
 // COMMON CONFIGURATION
 
 #define SPINSPIN_RANDOM_DELAY 50
-#define MOTOR_ONLINE_CHECK 	1
+#define MOTOR_ONLINE_CHECK 	-1
 //1 for annoying beep sound, 0 for some error beeps every 3s, -1 for absolute peace and tranquility (with pancik
 #define ARM_SWITCH 			0			//set to 1 to enable remote up and down arming switch
 #define FIRING_DISABLE		0			//set to 1 to stop firing
@@ -43,8 +43,8 @@
 
 //if no overrides in the respective configs
 #ifndef CONTROL_DEFAULT
-#define CONTROL_DEFAULT 		KEYBOARD_CTRL_MODE
-//#define CONTROL_DEFAULT			REMOTE_CTRL_MODE
+//#define CONTROL_DEFAULT 		KEYBOARD_CTRL_MODE
+#define CONTROL_DEFAULT			REMOTE_CTRL_MODE
 
 //#define CONTROL_DEFAULT			SBC_CTRL_MODE
 #endif
