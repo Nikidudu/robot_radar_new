92de886697a28b3638908bbf9130a4897a77169e
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2025-12-26 18:44:20 +0800

    update ID

--- Core/Tasks/Src/gimbal_control_task.c
+++ Core/Tasks/Src/gimbal_control_task.c
@@ -275,7 +275,7 @@ void send_current_to_yaw_motor() {
 	CAN_tx_message.IDE = CAN_ID_STD;
 	CAN_tx_message.RTR = CAN_RTR_DATA;
 	CAN_tx_message.DLC = 0x08;
-	CAN_tx_message.StdId = CAN_6020_1_TO_4_ID;
+	CAN_tx_message.StdId = CAN_6020_5_TO_8_ID;
 	if (g_safety_toggle || g_remote_cmd.right_switch == ge_RSW_SHUTDOWN) {
 		CAN_send_data[0] = 0;
 		CAN_send_data[1] = 0;
