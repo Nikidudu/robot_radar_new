2d3413e6f4519111eaeeb2439d6d8cc518ad38a3
Sze Kang <118478459+gskang-22@users.noreply.github.com>
2026-02-11 14:17:01 +0800

    udpate ioc file

--- Core/Inc/usart.h
+++ Core/Inc/usart.h
@@ -38,13 +38,11 @@ extern UART_HandleTypeDef huart3;
 
 extern UART_HandleTypeDef huart6;
 
+/* USER CODE BEGIN Private defines */
 extern DMA_HandleTypeDef hdma_usart1_rx;
 extern DMA_HandleTypeDef hdma_usart3_rx;
 extern DMA_HandleTypeDef hdma_usart6_rx;
 extern DMA_HandleTypeDef hdma_usart6_tx;
-
-/* USER CODE BEGIN Private defines */
-
 /* USER CODE END Private defines */
 
 void MX_USART1_UART_Init(void);
