03818bdce370cdc848887beb0981c8e467213fd3
Leaptail <52641374+Leaptail@users.noreply.github.com>
2026-03-10 22:52:46 +0800

    Mouse Y sense

--- Core/robot_config/robot_config_kirbee.h
+++ Core/robot_config/robot_config_kirbee.h
@@ -36,7 +36,7 @@
 #define REMOTE_PITCH_SPEED 	 	-0.1//0.005		//Speed of gimbal pitch turning
 
 #define MOUSE_X_SENSITIVITY		(300 * REMOTE_YAW_SPEED)				//Speed of yaw turning with mouse, dependent on above speed
-#define MOUSE_Y_SENSITIVITY 	(300 * REMOTE_PITCH_SPEED)				//Speed of pitch turning with mouse,  dependent on above speed
+#define MOUSE_Y_SENSITIVITY 	(150 * REMOTE_PITCH_SPEED)				//Speed of pitch turning with mouse,  dependent on above speed
 
 /*********************** MANUAL CONTROL CONFIGURATION *******************/
 //Inverts for both keyboard and mouse controls
